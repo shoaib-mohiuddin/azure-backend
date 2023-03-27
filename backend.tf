9ea24b6be1fe6fbe11f192b2bd7191d14b0d993d
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2023-03-27 15:56:33 +0530

    Runs pre-commit

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-#terraform {
-#  backend "azurerm" {
-#    resource_group_name  = "tfstate-rg"
-#    storage_account_name = "tfstateb5js3"
-#    container_name       = "blob-tfstateb5js3"
-#    key                  = "backend/terraform.tfstate"
-#  }
-#}
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstate7fax4"
+    container_name       = "blob-tfstate7fax4"
+    key                  = "backend/terraform.tfstate"
+  }
+}
